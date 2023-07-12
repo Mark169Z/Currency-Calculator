00b947645467f2f572fec56d4da4c1791fd61b8a
Mark169Z <90457798+Mark169Z@users.noreply.github.com>
2023-07-12 10:04:53 +0700

    Almost finished

---
+++ dist/script.js
@@ -0,0 +1,39 @@
+let rate=0;
+let rated=0;
+let ref=0;
+let TR={"JPY":0.008,"USD":0,"EUR":0.033,"GBP":0.036,"CNY":0.036};
+
+function GetCurrency(){
+    var val= document.getElementById("api-select").value;
+    var Currency = document.getElementById('Currency').value;
+    var checkbox = document.getElementById('checkbox');
+    console.log(checkbox.checked);
+    
+    fetch("https://open.er-api.com/v6/latest/THB", {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          rate = res.rates[val];
+          rated=1/rate;
+          if(val!="Country?"){
+          document.getElementById('Suffix').innerText = val;
+          }
+          if(Currency!="" && val!="Country?"){
+            if(checkbox.checked){
+              document.getElementById('Baht').value = `${((Currency * rated) - (Currency * rated * TR[val])).toFixed(2)} (${(Currency * rated * TR[val]).toFixed(2)} Refunded)`;
+            }
+            else{
+              document.getElementById('Baht').value = `${((Currency * rated)).toFixed(2)}`;
+            }
+          }
+          else if(Currency==""){
+            document.getElementById('Baht').value="";
+          }
+        })
+        .catch((err) => {
+          console.log(err, "Please Contact Developer");
+          rate = ERROR;
+        });
+}
+
